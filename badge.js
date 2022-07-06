8d793d394484648ddee68382989d8536e88283a1
Danny Vasconez <danny@vasconez.work>
2022-07-06 01:13:16 -0500

    Update badge.js

--- badge.js
+++ badge.js
@@ -1,20 +1,21 @@
 (function(){
-    var elements = document.getElementsByClassName("cert_badge");
+  const font_size_text="10px";  
+  var elements = document.getElementsByClassName("cert_badge");
     for(var i = 0; i < elements.length; i++) {
       const cert_canvas = elements[i];
       cert_canvas.setAttribute('style','display: inline-block;');
       cert_canvas.setAttribute('scrolling','no');
-      let cert_name =cert_canvas.getAttribute('cert_name');
-      if(cert_name.length > 18){
-        cert_name=cert_name.substring(0, 14)+'...';
-      }else{
-        cert_name=cert_name+'&nbsp;'.repeat(17-cert_name.length);
+      let cert_name =cert_canvas.getAttribute('cert_name').toUpperCase();
+      if(cert_name.length > 80){
+        cert_name=cert_name.substring(0, 77)+"...";
       }
-      let cert_issuer=cert_canvas.getAttribute('cert_issuer');
-      if(cert_issuer.length > 10){
-        cert_issuer='Issuer: '+cert_issuer.substring(0, 6)+'...';
-      }else{
-        cert_issuer='Issuer: '+cert_issuer+'&nbsp;'.repeat(9-cert_issuer.length);
+      let cert_type ="TYPE: "+cert_canvas.getAttribute('cert_type').toUpperCase();
+      if(cert_type.length > 80){
+        cert_type=cert_type.substring(0, 77)+"...";
+      }
+      let cert_issuer='ISSUER: '+cert_canvas.getAttribute('cert_issuer').toUpperCase();
+      if(cert_issuer.length > 80){
+        cert_issuer=cert_issuer.substring(0, 77)+"...";
       }
       let cert_logo=cert_canvas.getAttribute('cert_logo');
       let cert_href=cert_canvas.getAttribute('cert_href');
@@ -24,7 +25,7 @@
       cert_card.setAttribute('target','_blank');
       cert_card.setAttribute('rel','noopener noreferrer');
       const cert_body = document.createElement('div');
-      cert_body.setAttribute('style','text-align: center; min-width:150px; max-width: 150px; width: 150px; min-height: 250px; max-height: 250px; height: 250px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s; border: 2px solid grey; border-radius: 5px; padding: 5px;');
+      cert_body.setAttribute('style','text-align: center; min-width:150px; max-width: 150px; width: 150px; min-height: 250px; max-height: 250px; height: 250px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s; border: 2px solid grey; border-radius: 5px; padding: 5px; margin: 2px;');
       cert_body.setAttribute('onmouseover',"this.style.box-shadow='0 8px 16px 0 rgba(0,0,0,0.2);'");
       cert_body.setAttribute('onmouseout',"this.style.box-shadow=''");
       const cert_img = document.createElement('img');
@@ -32,7 +33,18 @@
       cert_img.setAttribute('style','height: 105px; width:105px;');
       const cert_container = document.createElement('div');
       cert_container.setAttribute('style','padding: 2px 16px;');
-      cert_container.innerHTML='<h4><b>'+cert_name+'</b></h4><p>'+cert_issuer+'</p>';
+      const cert_container_cert_name = document.createElement('div');
+      cert_container_cert_name.setAttribute('style','height: 80px; font-size: '+font_size_text+'; white-space: pre-wrap; word-wrap: break-word; overflow: hidden; text-overflow: ellipsis;');
+      cert_container_cert_name.innerHTML=cert_name
+      const cert_container_cert_type = document.createElement('div');
+      cert_container_cert_type.setAttribute('style','font-size: '+font_size_text+'; white-space: nowrap; word-wrap: break-word; overflow: hidden; text-overflow: ellipsis;');
+      cert_container_cert_type.innerHTML=cert_type
+      const cert_container_cert_issuer = document.createElement('div'); 
+      cert_container_cert_issuer.setAttribute('style','font-size: '+font_size_text+'; white-space: nowrap; word-wrap: break-word; overflow: hidden; text-overflow: ellipsis;');
+      cert_container_cert_issuer.innerHTML=cert_issuer
+      cert_container.appendChild(cert_container_cert_name);
+      cert_container.appendChild(cert_container_cert_type);
+      cert_container.appendChild(cert_container_cert_issuer);
       cert_body.appendChild(cert_img);
       cert_body.appendChild(cert_container);
       cert_card.appendChild(cert_body);
